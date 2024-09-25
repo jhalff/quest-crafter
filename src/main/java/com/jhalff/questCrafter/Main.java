bd7ea36fe052fc47272342d57b50c960f1aa1110
jhalff <j_halff@live.nl>
2024-09-25 19:30:56 +0200

    Improve help menu styling

--- src/main/java/com/jhalff/questCrafter/Main.java
+++ src/main/java/com/jhalff/questCrafter/Main.java
@@ -4,12 +4,13 @@ import com.jhalff.questCrafter.commands.BaseCommand;
 import com.jhalff.questCrafter.commands.CompassCommand;
 import com.jhalff.questCrafter.commands.HelpCommand;
 import com.jhalff.questCrafter.listeners.JoinListener;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
+
+import static com.jhalff.questCrafter.commands.HelpCommand.sendHelpMenu;
 
 public final class Main extends JavaPlugin {
 
@@ -17,10 +18,10 @@ public final class Main extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
 
-        BaseCommand<Main> baseCommand = new BaseCommand<Main>(this) {
+        BaseCommand<Main> baseCommand = new BaseCommand<>(this) {
             @Override
             public boolean runCommand(CommandSender sender, Command rootCommand, String label, String[] args) {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&4Help menu"));
+                sendHelpMenu(sender);
                 return true;
             }
         };
--- src/main/java/com/jhalff/questCrafter/commands/HelpCommand.java
+++ src/main/java/com/jhalff/questCrafter/commands/HelpCommand.java
@@ -7,13 +7,30 @@ import org.bukkit.command.CommandSender;
 
 public class HelpCommand extends BaseCommand<Main> {
 
-   public HelpCommand(Main plugin) {
-       super(plugin);
-   }
+    public HelpCommand(Main plugin) {
+        super(plugin);
+    }
+
+    public static void sendHelpMenu(CommandSender sender) {
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', generateHelpMenu()));
+    }
 
     @Override
     public boolean runCommand(CommandSender sender, Command rootCommand, String label, String[] args) {
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&4Help menu"));
+        sendHelpMenu(sender);
         return true;
+    }
+
+    private static String generateHelpMenu() {
+        return """
+                 \n
+                §e---------- &fQuestCrafter &e- &fCommand List &e----------
+                 \n
+                &6/quests help &7Open list of available commands
+                &6/quests compass&7 Spawn an official quests compass
+                 \n
+                §e----------------------------------------------
+                 \n
+                """;
     }
 }
